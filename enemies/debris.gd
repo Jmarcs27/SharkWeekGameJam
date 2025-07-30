5cec0c59b530eb6a75de8e96d180f0a7c98a1ceb
Nick Gibbons <141852042+ngibbons-uscs@users.noreply.github.com>
2025-07-30 18:00:02 -0400

    Add collision logic to obstacles

--- enemies/debris.gd
+++ enemies/debris.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 @export var moveSpeed = 200
+var hitPoints = 1
 
 func _physics_process(delta: float) -> void:
 	var velocity = Vector2.ZERO 
@@ -10,3 +11,17 @@ func _physics_process(delta: float) -> void:
 	if position.y >= 400:
 		print("Deleting Debris")
 		queue_free()   
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	print("Debris touched by ", area.name)
+	if area.name == "ShorkProjectile":
+		hitPoints -= 1
+		print("Debris Hit! HP Reamining: ", hitPoints)
+		if hitPoints <= 0:
+			kill_debris()
+			
+func kill_debris():
+	var position = transform
+	# TODO: spawn power ups
+	
+	queue_free()
